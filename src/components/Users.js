4b86f97b4acbc37d3c950c71bc1d6131fdea6a77
Ajmal777 <81432526+Ajmal777@users.noreply.github.com>
2023-09-01 16:44:04 +0000

    first commit

---
+++ src/components/Users.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+
+const Users = () =>{
+    const [userList, setUserList] = useState([]);
+
+    function fetchData(){
+        Axios.get('https://reqres.in/api/users')
+        .then(response => {
+            setUserList(response.data.data);
+        })
+        .catch(error => console.log(error));
+    }
+
+
+    return (
+        <div>
+            <button className="btn" onClick={fetchData}>Get User List</button>
+            <table>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
+                    <th>Avatar</th>
+                </tr>
+                {
+                    userList < 1 && 
+                    <tr>
+                        <td colSpan={4}>No data found to display</td>
+                    </tr>
+                }
+                {
+                    userList.length > 0 && 
+                    userList.map(user => {
+                        return (
+                            <tr>
+                                <td>{user.first_name}</td>
+                                <td>{user.last_name}</td>
+                                <td>{user.email}</td>
+                                <td>
+                                    <img src={user.avatar} style={{maxWidth: '20px', maxHeight: '20px'}} />
+                                </td>
+                            </tr>
+                        )
+                    })
+                }
+            </table>
+        </div>
+    )
+}
+
+export default Users;
