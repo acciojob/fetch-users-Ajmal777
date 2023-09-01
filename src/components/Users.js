543b720f26b9514b2db549110b779cce082fe242
Ajmal777 <81432526+Ajmal777@users.noreply.github.com>
2023-09-01 16:46:57 +0000

    second commit

--- src/components/Users.js
+++ src/components/Users.js
@@ -17,33 +17,37 @@ const Users = () =>{
         <div>
             <button className="btn" onClick={fetchData}>Get User List</button>
             <table>
-                <tr>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Email</th>
-                    <th>Avatar</th>
-                </tr>
-                {
-                    userList < 1 && 
+                <thead>
                     <tr>
-                        <td colSpan={4}>No data found to display</td>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                        <th>Avatar</th>
                     </tr>
-                }
-                {
-                    userList.length > 0 && 
-                    userList.map(user => {
-                        return (
-                            <tr>
-                                <td>{user.first_name}</td>
-                                <td>{user.last_name}</td>
-                                <td>{user.email}</td>
-                                <td>
-                                    <img src={user.avatar} style={{maxWidth: '20px', maxHeight: '20px'}} />
-                                </td>
-                            </tr>
-                        )
-                    })
-                }
+                </thead>
+                <tbody>
+                    {
+                        userList < 1 && 
+                        <tr>
+                            <td colSpan={4}>No data found to display.</td>
+                        </tr>
+                    }
+                    {
+                        userList.length > 0 && 
+                        userList.map(user => {
+                            return (
+                                <tr>
+                                    <td>{user.first_name}</td>
+                                    <td>{user.last_name}</td>
+                                    <td>{user.email}</td>
+                                    <td>
+                                        <img src={user.avatar} style={{maxWidth: '20px', maxHeight: '20px'}} />
+                                    </td>
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
             </table>
         </div>
     )
